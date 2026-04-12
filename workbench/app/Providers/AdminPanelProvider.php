c1d9de441fa635636344a9da8a56cfb6ab35c0d1
ziming <ziming@users.noreply.github.com>
2026-04-12 18:49:11 +0800

    Avoid deprecated csrf middleware constant

--- workbench/app/Providers/AdminPanelProvider.php
+++ workbench/app/Providers/AdminPanelProvider.php
@@ -9,7 +9,7 @@ use Filament\Panel;
 use Filament\PanelProvider;
 use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 use Illuminate\Cookie\Middleware\EncryptCookies;
-use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
+use Illuminate\Foundation\Http\Middleware\ValidateCsrfToken;
 use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
@@ -31,7 +31,9 @@ class AdminPanelProvider extends PanelProvider
                 StartSession::class,
                 AuthenticateSession::class,
                 ShareErrorsFromSession::class,
-                VerifyCsrfToken::class,
+                class_exists(ValidateCsrfToken::class)
+                    ? ValidateCsrfToken::class
+                    : 'Illuminate\Foundation\Http\Middleware\VerifyCsrfToken',
                 SubstituteBindings::class,
                 DisableBladeIconComponents::class,
                 DispatchServingFilamentEvent::class,
